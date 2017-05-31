c640ad515a049afefa29e396912a7d62bed4ec0f
Antoine Legrand <legran_e@epita.fr>
2017-05-31 16:29:47 +0200

    [FRAGMENTS] Added all fragments classes

---
+++ app/src/main/java/fr/epita/epilifetoday/AboutFragment.java
@@ -0,0 +1,45 @@
+package fr.epita.epilifetoday;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by antoine on 31/05/2017.
+ */
+
+public class AboutFragment extends Fragment {
+    AboutFragment.AboutInterface aboutInterface;
+
+    public AboutFragment() {
+
+    }
+
+    public interface AboutInterface {
+        public void back();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return null;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
---
+++ app/src/main/java/fr/epita/epilifetoday/TimelineFragment.java
@@ -0,0 +1,44 @@
+package fr.epita.epilifetoday;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by antoine on 31/05/2017.
+ */
+
+public class TimelineFragment extends Fragment {
+    TimelineFragment.TimelineInterface timelineInterface;
+
+    public TimelineFragment() {
+
+    }
+
+    public interface TimelineInterface {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return null;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
